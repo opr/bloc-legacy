095dce20a9d2a4733f0713689a4eef8ea0b71752
Richard <richard@edgethreesixty.com>
2017-02-16 17:21:11 +0000

    Added breadcrumbs

---
+++ components/breadcrumbs.php
@@ -0,0 +1,8 @@
+<div class="breadcrumbs">
+    <div class="hidden-label" id="breadcrumbs__label">You are here:</div>
+    <ol class="breadcrumbs__list" aria-labelledby="breadcrumbs__label">
+        <li class="breadcrumbs__list__item"><a href="/" rel="up up">Home</a></li>
+            <li class="breadcrumbs__list__item"><a href="/news/" clas="breadcrumbs__list__item__link" rel="&quot;up&quot;">News</a></li>
+            <li class="breadcrumbs__list__item"><a href="/news/health-videos-accessible-on-dobsons-choice/" clas="breadcrumbs__list__item__link">Health videos accessible on Dobson's Choice</a></li>
+    </ol>
+</div>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="assets/styles/css/bloc.css"/>
 	<link rel="stylesheet" type="text/css" href="http://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css"/>
 </head>
@@ -14,6 +15,7 @@
 	<?php include "components/footer.php" ?>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.6.15/browser-polyfill.min.js"></script>
 <script src="assets/js/dist/bloc.js"></script>
 <script src="assets/js/dist/bundle.min.js"></script>
 <script type="text/javascript" src="http://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
