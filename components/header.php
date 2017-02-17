f1410004a948de3cc902ae823b594acb9dc334d7
Richard <richard@edgethreesixty.com>
2017-02-17 17:34:27 +0000

    Updated head

--- components/header.php
+++ components/header.php
@@ -2,14 +2,14 @@
 	<div class="header__logo">
 		<div class="header__logo__image"></div>
 	</div>
-	<nav class="header__navigation">
-		<ul class="header__navigation__menu">
-			<li class="header__navigation__menu__item"><a href="#" class="header__navigation__menu__item__link">Home</a></li>
-			<li class="header__navigation__menu__item"><a href="#" class="header__navigation__menu__item__link">Menu item 1</a></li>
-			<li class="header__navigation__menu__item"><a href="#" class="header__navigation__menu__item__link">Menu item 2</a></li>
-			<li class="header__navigation__menu__item"><a href="#" class="header__navigation__menu__item__link">Menu item 3</a></li>
-			<li class="header__navigation__menu__item"><a href="#" class="header__navigation__menu__item__link">Menu item 4</a></li>
-			<li class="header__navigation__menu__item"><a href="#" class="header__navigation__menu__item__link">Menu item 5</a></li>
+	<nav class="header__navigation" role="navigation">
+		<ul class="header__navigation__menu" role="menubar">
+			<li class="header__navigation__menu__item" role="presentation"><a href="#" class="header__navigation__menu__item__link" role="menuitem">Home</a></li>
+			<li class="header__navigation__menu__item" role="presentation"><a href="#" class="header__navigation__menu__item__link" role="menuitem">Menu item 1</a></li>
+			<li class="header__navigation__menu__item" role="presentation"><a href="#" class="header__navigation__menu__item__link" role="menuitem">Menu item 2</a></li>
+			<li class="header__navigation__menu__item" role="presentation"><a href="#" class="header__navigation__menu__item__link" role="menuitem">Menu item 3</a></li>
+			<li class="header__navigation__menu__item" role="presentation"><a href="#" class="header__navigation__menu__item__link" role="menuitem">Menu item 4</a></li>
+			<li class="header__navigation__menu__item" role="presentation"><a href="#" class="header__navigation__menu__item__link" role="menuitem">Menu item 5</a></li>
 		</ul>
 	</nav>
 </header>
