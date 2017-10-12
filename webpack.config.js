cb89e1c1675e072e1e639ed4e642961090312e3a
opr <opr@users.noreply.github.com>
2017-10-12 14:33:32 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
 var webpack = require('webpack');
 var path = require('path');
 console.log(
-    path.join(__dirname, '/assets-new/js/react/', 'index.jsx'));
+    path.join(__dirname, '/assets/js/react/', 'index.jsx'));
 module.exports = {
     devtool: '#eval-source-map',
 
     entry: [
         'webpack/hot/dev-server?reload=true',
         'webpack-hot-middleware/client',
-        path.join(__dirname, '/assets-new/js/react/', 'index.jsx')
+        path.join(__dirname, '/assets/js/react/', 'index.jsx')
     ],
 
     output: {
-        path: path.join(__dirname, '/assets-new/js', 'dist'),
-        publicPath: '/assets-new/js/dist/',
+        path: path.join(__dirname, '/assets/js', 'dist'),
+        publicPath: '/assets/js/dist/',
         filename: 'bundle.min.js'
     },
 
@@ -24,7 +24,7 @@ module.exports = {
 
     module: {
         loaders: [
-            { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['react-hot-loader', 'babel-loader'] }
+            { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['react-hot-loader/webpack', 'babel-loader'] }
         ]
     },
     resolve: {
